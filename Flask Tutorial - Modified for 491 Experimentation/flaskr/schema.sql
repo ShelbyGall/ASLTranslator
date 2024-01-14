faf0d2c82cdbc6da868cb06b8b32cafcff0633f4
AH-Neuro <125938682+AH-Neuro@users.noreply.github.com>
2024-01-13 16:01:41 -0800

    Add files via upload

--- Flask Tutorial - Modified for 491 Experimentation/flaskr/schema.sql
+++ Flask Tutorial - Modified for 491 Experimentation/flaskr/schema.sql
@@ -1,3 +1,28 @@
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` text,
+  `password` longtext,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `usercol` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  `mobilenumber` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+
+CREATE TABLE `post` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `author_id` int NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `title` text NOT NULL,
+  `body` longtext,
+  PRIMARY KEY (`id`),
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`)
+)
+
+/*
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
 
@@ -20,3 +45,4 @@ CREATE TABLE post (
     body TEXT NOT NULL,
     FOREIGN KEY (author_id) REFERENCES user (id)
 );
+*/
