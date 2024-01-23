80a2810f01c688a53554fdc0ea708c3439d1bbfd
Shelby Gallegos <116443555+ShelbyGall@users.noreply.github.com>
2024-01-23 14:11:55 -0800

    Delete Flask Tutorial - Modified for 491 Experimentation/splash.py

--- Flask Tutorial - Modified for 491 Experimentation/splash.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-
-bp = Blueprint('splash', __name__, url_prefix='/splash')
-
-@bp.route('/', methods=('GET', 'POST'))
-def splash():
-    return render_template('splash.html')
