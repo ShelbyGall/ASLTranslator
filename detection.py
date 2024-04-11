4cc7ccebbf1a324d4e6859dfcd736b73372ceac7
Shelby Gallegos <sjgallegos270@gmail.com>
2024-04-11 15:06:49 -0700

    got camera label working to detect camera is active

--- detection.py
+++ detection.py
@@ -2,6 +2,8 @@ import PredictLetter as pl
 import tkinter as tk
 from PIL import ImageTk, Image
 import cv2
+import time
+
 
 sentence = []
 
@@ -9,33 +11,45 @@ def clearSen():
     if sentence:
         sentence.clear()
 
-def streamVideo(counter):
-    _, frame = cap.read()
-    if (counter % 30) == 0:
-        letter = pl.predict_letter_from_image(frame)
-        if letter == 'del':
-            if sentence:
-                sentence.pop()
-        elif letter == "space":
-            sentence.append("_")
-        elif letter != 'nothing':
-            sentence.append(letter)
-        text.configure(text="".join(sentence))
+def camCheck():
+    if cap.isOpened():
+        camLab.config(text="Camera Online", fg="white", bg="green")
+    else:
+        camLab.config(text="Camera Offline", fg="white", bg="red")
+    
+    return cap.isOpened()
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    frame = Image.fromarray(frame)
-    ftk = ImageTk.PhotoImage(frame)
-    detectLab.ftk = ftk
-    detectLab.configure(image=ftk)
+
+def streamVideo(counter):
+    if camCheck():
+        _, frame = cap.read()
+        if (counter % 30) == 0:
+            letter = pl.predict_letter_from_image(frame)
+            if letter == 'del':
+                if sentence:
+                    sentence.pop()
+            elif letter == "space":
+                sentence.append("_")
+            elif letter != 'nothing':
+                sentence.append(letter)
+            text.configure(text="".join(sentence))
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        frame = Image.fromarray(frame)
+        ftk = ImageTk.PhotoImage(frame)
+        detectLab.ftk = ftk
+        detectLab.configure(image=ftk)
     counter += 1
     detectLab.after(1, streamVideo, counter)
 
     
 
 detectWin = tk.Tk()
-detectWin.geometry('800x800')
+detectWin.geometry('800x600')
 detectWin.title("ASL detector")
 
+camLab = tk.Label(detectWin)
+camLab.pack()
 
 text = tk.Label(text="here is the output")
 text.pack()
@@ -46,9 +60,7 @@ clear.pack()
 detectLab = tk.Label(detectWin)
 detectLab.pack()
 
-
 cap = cv2.VideoCapture(0)
- 
 
 streamVideo(counter=0)
 
